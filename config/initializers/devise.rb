9937ee719d1a5cc8ce558c396114fdaa6b9054ca
Garen Torikian <gjtorikian@gmail.com>
2014-07-30 16:47:30 -0700

    Set scope for web hooks

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['GITHUB_BRANTA_CLIENT_ID'], ENV['GITHUB_BRANTA_CLIENT_SECRET'], scope: 'user,public_repo'
+  config.omniauth :github, ENV['GITHUB_BRANTA_CLIENT_ID'], ENV['GITHUB_BRANTA_CLIENT_SECRET'], scope: 'write:repo_hook'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
