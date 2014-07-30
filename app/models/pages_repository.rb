7e94bb3551984b4fce2411a6e30c6d258a6ce7ca
Garen Torikian <gjtorikian@gmail.com>
2014-07-30 16:47:40 -0700

    Add a UI for adding web hooks

--- app/models/pages_repository.rb
+++ app/models/pages_repository.rb
@@ -6,4 +6,8 @@ class PagesRepository
   def pages_repos
     @pages_repos ||= @repos.select { |repo| repo["name"] =~ /page/i }
   end
+
+  def has_hook_to_granta?(client, repo_nwo)
+    !client.hooks(repo_nwo).select { |hook| hook["config"]["url"] =~ /granta|0\.0\.0\.0/ }.empty?
+  end
 end
