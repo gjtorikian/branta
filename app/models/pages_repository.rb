7586ba0e4f7389f47defe3a84c5b47724e34ddc7
Garen Torikian <gjtorikian@gmail.com>
2014-08-07 21:33:55 -0700

    Cleanup repository listings

--- app/models/pages_repository.rb
+++ app/models/pages_repository.rb
@@ -1,24 +1,13 @@
 require 'uri'
 
-class PagesRepository
-  def initialize(payload)
-    @repos = payload
+class PagesRepository < ActiveRecord::Base
+  validates_presence_of :name_with_owner
+
+  def self.pages_repos(payload)
+    @pages_repos ||= payload.select { |repo| repo["name"] =~ /page/i }
   end
 
-  def pages_repos
-    @pages_repos ||= @repos.select { |repo| repo["name"] =~ /page/i }
-  end
-
-  def has_hook_to_granta?(hooks)
-    hooks.select do |hook|
-      url = hook["config"]["url"]
-      next if url.nil?
-      uri = URI.parse(url)
-      if Rails.env.production? || Rails.env.test?
-        return uri.host == "www.branta.com"
-      else
-        return uri.host == "www.branta.com" || uri.host == "0.0.0.0"
-      end
-    end.present?
+  def self.has_hook_to_granta?(repo_name)
+    !PagesRepository.find_by_name_with_owner(repo_name).nil?
   end
 end
