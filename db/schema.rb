06c6e09596835d3274508f87f98782901af496db
Garen Torikian <gjtorikian@gmail.com>
2014-08-07 21:33:07 -0700

    Construct a new PagesRepository model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730022859) do
+ActiveRecord::Schema.define(version: 20140808034935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20140730022859) do
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "pages_sites", force: true do |t|
-    t.string   "name"
+  create_table "pages_repositories", force: true do |t|
+    t.string   "name_with_owner", default: "", null: false
+    t.integer  "hook_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
