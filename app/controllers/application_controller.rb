6028a4f397ae082bb61194ba4bef2aff8a5e1296
Garen Torikian <gjtorikian@gmail.com>
2014-08-23 11:58:51 -0700

    Trash this unneeded var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @pages_builds = PagesBuild.limit(50).reverse_order
   end
 end
--- app/view_models/pages_builds/index_view.rb
+++ app/view_models/pages_builds/index_view.rb
@@ -2,15 +2,14 @@ module PagesBuilds
   class IndexView
     attr_reader :pages_builds, :user_pages_builds
 
-    def initialize(pages_builds)
-      @pages_builds = pages_builds
+    def initialize
     end
 
     def empty_for_user?(user)
       repo_ids = Repository.where(:owner => user.login).pluck(:id)
       return true if repo_ids.empty?
       @user_pages_builds = repo_ids.map do |repo_id|
-        PagesBuild.where(:repository_id => repo_id)
+        PagesBuild.where(:repository_id => repo_id).limit(50)
       end
       ap @user_pages_builds
       @user_pages_builds.empty?
