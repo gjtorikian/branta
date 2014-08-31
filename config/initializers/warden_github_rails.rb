bfa4e3e1121bf8a945e3889f0bd2e85aeb750cc6
Garen Torikian <gjtorikian@gmail.com>
2014-08-30 23:15:35 -0700

    Modify warden scopes to allow for hook admin

--- config/initializers/warden_github_rails.rb
+++ config/initializers/warden_github_rails.rb
@@ -3,7 +3,7 @@ require 'warden/github/rails'
 Warden::GitHub::Rails.setup do |config|
   config.add_scope :user,  :client_id     => ENV['GITHUB_BRANTA_CLIENT_ID'],
                            :client_secret => ENV['GITHUB_BRANTA_CLIENT_SECRET'],
-                           :scope         => ["user:email"]
+                           :scope         => "user:email, admin:repo_hook"
 
   # config.add_team :employees, ENV['GITHUB_TEAM_ID'] || '696075'
 
