517fc41ed78010843a593d260a54d4e074e089bd
Garen Torikian <gjtorikian@gmail.com>
2014-08-30 18:38:58 -0700

    Split and spruce up app pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,20 +10,24 @@ class ResqueWhitelistConstraint
 end
 
 Branta::Application.routes.draw do
-  root 'application#index'
-
   post "/post_receive"         => "webhook#create"
   get "/search"                => "search#index"
 
   if ENV['GITHUB_BRANTA_ORG_NAME'].nil?
+    root 'application#index'
+
     get "/login"               => "sessions#create"
     get "/logout"              => "sessions#destroy"
+    get "/add_hook"            => "application#add_hook"
 
     mount Resque::Server.new, :at => "/resque", constraints: ResqueWhitelistConstraint.new
   else
     github_authenticate(:org => ENV['GITHUB_BRANTA_ORG_NAME']) do
+      root 'application#index'
+
       get "/login"               => "sessions#create"
       get "/logout"              => "sessions#destroy"
+      get "/add_hook"            => "application#add_hook"
 
       mount Resque::Server.new, :at => "/resque"
     end
