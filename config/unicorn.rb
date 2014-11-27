1172a9969328bd3315eb35e543df8c94794b3354
Garen Torikian <gjtorikian@gmail.com>
2014-11-26 16:51:27 -0800

    Merge master

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,27 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "../public"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "./pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/logs/unicorn.log"
+# stdout_path "/path/to/logs/unicorn.log"
+stderr_path "./log/unicorn.log"
+stdout_path "./log/unicorn.log"
+
+# Unicorn socket
+# listen "/tmp/unicorn.[app name].sock"
 listen "/tmp/unicorn.branta.sock"
-timeout 15
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
 preload_app true
 
 before_fork do |server, worker|
